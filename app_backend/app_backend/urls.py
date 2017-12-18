14613342da88e40d22bfefa2977468bb4455be9a
Rohan Datta <iamrohandatta@gmail.com>
2017-12-18 16:09:12 +0530

    Initialised Frontend and Added Signup API

--- app_backend/app_backend/urls.py
+++ app_backend/app_backend/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.static import serve
 from portal.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index),
+    url(r'^api/$', index),
+    url(r'^api/signup/$', signup),
+    url(r'^(?P<path>.*)$', serve, {'document_root':'../app_frontend'}),
 ]
--- app_backend/portal/views.py
+++ app_backend/portal/views.py
@@ -1,6 +1,48 @@
 from __future__ import unicode_literals
-
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render_to_response
+from django.contrib.auth import authenticate,login,logout
+
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import *
+import os
+from random import randint
+from datetime import datetime
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+
+
+def email(to,subject,body):
+    body = body + "\n\n -------------------------------------\n\n"+" This is an Auto Generated Email. Please do not reply."
+    # print body
+    send_mail(subject,body,'"ARC Portal" <noreply@arc-portal.com>',[to],
+                          fail_silently=False)
 
 def index(request):
 	return render(request, 'index.html')
+
+@csrf_exempt
+def signup(request):
+	if isinstance(request.body, bytes):
+		data = json.loads(request.body.decode(encoding='utf-8'))
+	else:
+		data = json.loads(request.body)
+	username = data['username']
+	rollnumber = data['rollno']
+	random_num = str(randint(1,9))+str(randint(10,99))+str(randint(1,9))
+	password = random_num
+	error = "None"
+	if rollnumber != False:
+		rollnumber = str(rollnumber)
+		if User.objects.filter(username=username).exists():
+			error = "User already exists"
+		else:
+			email_user = rollnumber+'@iitk.ac.in'
+			user = User.objects.create_user(username=username,password=password,email=email_user)
+			user.save()
+			return JsonResponse({ 'username': username, 'rollnumber': rollnumber, 'password': password })
+
+	return JsonResponse({ 'error': error }, status=500)
